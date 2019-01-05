0ddb1ae7c0dcf74eb58969472aecea3860d8939d
Benjamin M Lancaster <bmlancaster07@gmail.com>
2019-01-05 21:40:58 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,5 +9,15 @@ class School
   end
   
   def add_student(student,level)
-    
+   roster[level] ||= []
+    roster[level] << student
+  end 
+  
+  def grade(level)
+    roster.detect do |x, y| 
+      if x == level
+        return y 
+      end 
+    end 
+  end  
     
