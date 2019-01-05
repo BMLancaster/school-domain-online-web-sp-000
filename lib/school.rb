c3d04b6277c5e48a595665db181f82e36c7c87eb
Benjamin M Lancaster <bmlancaster07@gmail.com>
2019-01-05 21:41:21 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,15 +1,17 @@
 # code here!
 
-class School 
+class School
   attr_accessor :name, :roster
   
-  def intialize(name)
+  def initialize(name)
     @name = name
-    @roster = roster {}
-  end
+    @roster = {}
+  end 
   
-  def add_student(student,level)
-   roster[level] ||= []
+  def add_student(student, level)
+    # this sort of means #=> a || a = b ... or #=> x || x = y
+    # it's freak'n weird
+    roster[level] ||= []
     roster[level] << student
   end 
   
@@ -19,5 +21,5 @@ class School
         return y 
       end 
     end 
-  end  
+  end 
     
